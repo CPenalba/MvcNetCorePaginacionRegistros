9aad1ed7320e859b45ac1e43a452240da8dbec5d
CPenalba <carolinapenalbacorpa@gmail.com>
2025-03-06 14:18:24 +0100

    Todo terminado dia 06/03

--- MvcNetCorePaginacionRegistros/Controllers/PaginacionController.cs
+++ MvcNetCorePaginacionRegistros/Controllers/PaginacionController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Elfie.Diagnostics;
 using MvcNetCorePaginacionRegistros.Models;
 using MvcNetCorePaginacionRegistros.Repositories;
 
@@ -48,26 +49,102 @@ namespace MvcNetCorePaginacionRegistros.Controllers
             {
                 posicion = 1;
             }
-            //AHORA DEBEMOS DIBUJAR NUMEROS DE PAGINA
-            //QUE DEPENDEN DEL NUMERO DE REGISTROS
-            //<a href='paginacion?posicion=1'>Página 1</a>
-            //<a href='paginacion?posicion=3'>Página 2</a>
-            //<a href='paginacion?posicion=5'>Página 3</a>
-            int numeroPagina = 1;
+            ////AHORA DEBEMOS DIBUJAR NUMEROS DE PAGINA
+            ////QUE DEPENDEN DEL NUMERO DE REGISTROS
+            ////<a href='paginacion?posicion=1'>Página 1</a>
+            ////<a href='paginacion?posicion=3'>Página 2</a>
+            ////<a href='paginacion?posicion=5'>Página 3</a>
+            //int numeroPagina = 1;
             int numRegistros = await this.repo.GetNumeroRegistrosVistaDepartamentosAsync();
             //VAMOS A REALIZAR UN DIBUJO DINAMICO CON 
             //HTML Y SE LO ENVIAMOS A LA VISTA
-            string html = "<div>";
-            for (int i = 1; i <= numRegistros; i += 2)
-            {
-                html += "<a href='PaginarGrupoVistaDepartamento?posicion="
-                    + i + "'>Página " + numeroPagina + "</a> | ";
-                numeroPagina += 1;
-            }
-            html += "</div>";
-            ViewData["LINKS"] = html;
+            //string html = "<div>";
+            //for (int i = 1; i <= numRegistros; i += 2)
+            //{
+            //    html += "<a href='PaginarGrupoVistaDepartamento?posicion="
+            //        + i + "'>Página " + numeroPagina + "</a> | ";
+            //    numeroPagina += 1;
+            //}
+            //html += "</div>";
+            ViewData["REGISTROS"] = numRegistros;
             List<VistaDepartamento> departamentos = await this.repo.GetGrupoVistaDepartamentoAsync(posicion.Value);
             return View(departamentos);
+        }
+
+        public async Task<IActionResult> PaginarGrupoDepartamentos(int? posicion)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+            }
+            int numRegistros = await this.repo.GetNumeroRegistrosVistaDepartamentosAsync();
+            ViewData["REGISTROS"] = numRegistros;
+            List<Departamento> departamentos = await this.repo.GetGrupoDepartamentosAsync(posicion.Value);
+            return View(departamentos);
+        }
+
+        public async Task<IActionResult> PaginarGrupoEmpleados(int? posicion)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+            }
+
+            int registros = await this.repo.GetEmpleadosCountAsync();
+            ViewData["REGISTROS"] = registros;
+            List<Empleado> empleados = await this.repo.GetGrupoEmpleadosAsync(posicion.Value);
+            return View(empleados);
+        }
+
+        public async Task<IActionResult> EmpleadosOficio(int? posicion, string oficio)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+                return View();
+            }
+            else
+            {
+                List<Empleado> empleados = await this.repo.GetEmpleadosOficioAsync(posicion.Value, oficio);
+                int registros = await this.repo.GetEmpleadosOficioCountAsync(oficio);
+                ViewData["REGISTROS"] = registros;
+                ViewData["OFICIO"] = oficio;
+                return View(empleados);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> EmpleadosOficio(string oficio)
+        {
+            List<Empleado> empleados = await this.repo.GetEmpleadosOficioAsync(1, oficio);
+            int registros = await this.repo.GetEmpleadosOficioCountAsync(oficio);
+            ViewData["REGISTROS"] = registros;
+            ViewData["OFICIO"] = oficio;
+            return View(empleados);
+        }
+
+        public async Task<IActionResult> EmpleadosOficioOut(int? posicion, string oficio)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+                return View();
+            }
+            else
+            {
+                ModelEmpleadosOficio model = await this.repo.GetEmpleadosOficioOutAsync(posicion.Value, oficio);
+                ViewData["REGISTROS"] = model.NumeroRegistros;
+                ViewData["OFICIO"] = oficio;
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EmpleadosOficioOut(string oficio)
+        {
+            ModelEmpleadosOficio model = await this.repo.GetEmpleadosOficioOutAsync(1, oficio);
+            ViewData["REGISTROS"] = model.NumeroRegistros;
+            ViewData["OFICIO"] = oficio;
+            return View(model);
         }
     }
 }
--- MvcNetCorePaginacionRegistros/Repositories/RepositoryHospital.cs
+++ MvcNetCorePaginacionRegistros/Repositories/RepositoryHospital.cs
@@ -1,13 +1,61 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 using MvcNetCorePaginacionRegistros.Data;
 using MvcNetCorePaginacionRegistros.Models;
+using System.Data;
 
 #region
 //CREATE VIEW V_DEPARTAMENTOS_INDIVIDUAL
 //AS
 //SELECT CAST(
 //ROW_NUMBER() OVER (ORDER BY DEPT_NO)AS INT) AS POSICION, DEPT_NO, DNOMBRE, LOC FROM DEPT
+//GO
+
+
+//CREATE PROCEDURE SP_GRUPO_DEPARTAMENTOS
+//(@posicion int)
+//AS
+//SELECT DEPT_NO, DNOMBRE, LOC FROM V_DEPARTAMENTOS_INDIVIDUAL
+//WHERE POSICION >= @posicion AND POSICION < (@posicion + 2)
+//GO
+
+//CREATE VIEW V_GRUPO_EMPLEADOS
+//AS
+//SELECT CAST(
+//ROW_NUMBER() OVER (ORDER BY APELLIDO)AS INT) AS POSICION, EMP_NO, APELLIDO, OFICIO, SALARIO, DEPT_NO FROM EMP
+//GO
+
+//CREATE PROCEDURE SP_GRUPO_EMPLEADOS
+//(@posicion int)
+//AS
+//SELECT EMP_NO, APELLIDO, OFICIO, SALARIO, DEPT_NO FROM V_GRUPO_EMPLEADOS
+//WHERE POSICION >= @posicion AND POSICION < (@posicion + 3)
+//GO
+
+//CREATE PROCEDURE SP_GRUPO_EMPLEADOS_OFICIO
+//(@posicion int, @oficio nvarchar(50))
+//AS
+//SELECT EMP_NO, APELLIDO, OFICIO, SALARIO, DEPT_NO FROM
+//(SELECT ROW_NUMBER() OVER (ORDER BY APELLIDO) AS POSICION, EMP_NO, APELLIDO, OFICIO, SALARIO, DEPT_NO FROM EMP
+//WHERE OFICIO = @oficio) QUERY WHERE POSICION >= @posicion AND POSICION < (@posicion +2)
+//GO
+
+//CREATE PROCEDURE SP_GRUPO_EMPLEADOS_OFICIO_OUT
+//(@posicion INT,
+//@oficio NVARCHAR(50),
+//@registros INT OUT)
+//AS
+//	SELECT @registros = COUNT(EMP_NO) FROM EMP
+//	WHERE OFICIO = @oficio
+//	SELECT EMP_NO, APELLIDO, OFICIO, SALARIO, DEPT_NO FROM (
+//		SELECT CAST(
+//		ROW_NUMBER() OVER (ORDER BY APELLIDO) AS INT) AS POSICION
+//		, EMP_NO, APELLIDO, OFICIO, SALARIO, DEPT_NO
+//		FROM EMP WHERE OFICIO = @oficio) QUERY
+//		WHERE POSICION >= @posicion AND 
+//		POSICION < (@posicion +2)
 //GO
 #endregion
 
@@ -60,6 +108,60 @@ namespace MvcNetCorePaginacionRegistros.Repositories
                            && datos.Posicion < (posicion + 2)
                            select datos;
             return await consulta.ToListAsync();
+        }
+
+        public async Task<List<Departamento>> GetGrupoDepartamentosAsync(int posicion)
+        {
+            string sql = "SP_GRUPO_DEPARTAMENTOS @posicion";
+            SqlParameter pamPosicion = new SqlParameter("@posicion", posicion);
+            var consulta = this.context.Departamentos.FromSqlRaw(sql, pamPosicion);
+            return await consulta.ToListAsync();
+        }
+        public async Task<int> GetEmpleadosCountAsync()
+        {
+            return await this.context.Empleados.CountAsync();
+        }
+
+        public async Task<List<Empleado>> GetGrupoEmpleadosAsync(int posicion)
+        {
+            string sql = "SP_GRUPO_EMPLEADOS @posicion";
+            SqlParameter pamPosicion = new SqlParameter("@posicion", posicion);
+            var consulta = this.context.Empleados.FromSqlRaw(sql, pamPosicion);
+            return await consulta.ToListAsync();
+        }
+
+        public async Task<int> GetEmpleadosOficioCountAsync(string oficio)
+        {
+            return await this.context.Empleados
+                .Where(z => z.Oficio == oficio).CountAsync();
+        }
+
+        public async Task<List<Empleado>> GetEmpleadosOficioAsync(int posicion, string oficio)
+        {
+            string sql = "SP_GRUPO_EMPLEADOS_OFICIO @posicion, @oficio";
+            SqlParameter pamPosicion = new SqlParameter("@posicion", posicion);
+            SqlParameter pamOficio = new SqlParameter("@oficio", oficio);
+            var consulta = this.context.Empleados.FromSqlRaw(sql, pamPosicion, pamOficio);
+            return await consulta.ToListAsync();
+        }
+
+        public async Task<ModelEmpleadosOficio> GetEmpleadosOficioOutAsync(int posicion, string oficio)
+        {
+            string sql = "SP_GRUPO_EMPLEADOS_OFICIO_OUT @posicion, @oficio, @registros out";
+            SqlParameter pamPosicion = new SqlParameter("@posicion", posicion);
+            SqlParameter pamOficio = new SqlParameter("@oficio", oficio);
+            SqlParameter pamRegistros = new SqlParameter("@registros", 0);
+            pamRegistros.Direction = ParameterDirection.Output;
+            var consulta = this.context.Empleados.FromSqlRaw
+                (sql, pamPosicion, pamOficio, pamRegistros);
+            //PRIMERO DEBEMOS EJECUTAR LA CONSULTA PARA PODER RECUPERAR LOS PARAMETROS DE SALIDA
+            List<Empleado> empleados = await consulta.ToListAsync();
+            int registros = int.Parse(pamRegistros.Value.ToString());
+            return new ModelEmpleadosOficio
+            {
+                NumeroRegistros = registros,
+                Empleados = empleados
+            };
         }
     }
 }
